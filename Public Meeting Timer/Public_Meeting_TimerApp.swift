c0fd95095bc489fddc79546f3c71f624a2b975cf
Craig R. Hughes <craig.git@rungie.com>
2020-12-11 09:23:33 -0800

    Initial Commit

---
+++ Public Meeting Timer/Public_Meeting_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Public_Meeting_TimerApp.swift
+//  Public Meeting Timer
+//
+//  Created by Craig Hughes on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Public_Meeting_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
